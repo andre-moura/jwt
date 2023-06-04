50aa5624f6be3874bc6a1b991034c02f259849c1
andre-moura <61800450+andre-moura@users.noreply.github.com>
2023-06-04 11:10:58 -0300

    Adding user model

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,23 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 from config import Config
 
+db = SQLAlchemy()
+migrate = Migrate()
 
-app = Flask(__name__)
-
-if __name__ == '__main__':
+def create_app():
+    app = Flask(__name__)
     app.config.from_object(Config)
-    app.run()
+
+    db.init_app(app)
+    migrate.init_app(app, db)
+
+    # Import models here to ensure they are registered with SQLAlchemy
+    from app.models import User
+
+    # Register blueprints/routes here
+    from app.routes.auth import auth_bp
+    app.register_blueprint(auth_bp)
+
+    return app
